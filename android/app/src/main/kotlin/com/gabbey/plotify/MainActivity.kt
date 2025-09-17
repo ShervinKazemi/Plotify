0960b251041833682a6d73b29686c06e3561fcf6
Shervin <github.kazmy2@gmail.com>
2025-09-17 21:43:57 +0330

    initial commit

---
+++ android/app/src/main/kotlin/com/gabbey/plotify/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.gabbey.plotify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:plotify/app.dart';
+
+void main() {
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  FlutterNativeSplash.remove();
+  runApp(const App());
+}
